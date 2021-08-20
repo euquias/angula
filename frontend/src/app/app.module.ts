5b00d97edf5fc110c9f70836b73958d51ca3a116
euquias <willmagalhaes25@gmail.com>
2021-08-20 13:58:34 -0300

    inserindo home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,9 +8,15 @@ import { HeaderComponent } from './components/template/header/header.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
+
 import {MatSidenavModule}from'@angular/material/sidenav'
 import {MatListModule}from'@angular/material/list';
 import { HomeComponent } from './views/home/home.component'
+
+import {MatCardModule}from'@angular/material/card'
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +31,8 @@ import { HomeComponent } from './views/home/home.component'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
